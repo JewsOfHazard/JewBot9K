2fa4f4acb3f8538a31993acdde93884953123e57
Logan Saso <jewsofhazard@gmail.com>
2016-01-12 01:29:27 -0800

    Added bleeding branch

--- JewBot9K/JewBot9K.Designer.cs
+++ JewBot9K/JewBot9K.Designer.cs
@@ -172,7 +172,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(683, 351);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "NonInitialized";
+            this.tabPage2.Text = "NextUP!";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // timer1
