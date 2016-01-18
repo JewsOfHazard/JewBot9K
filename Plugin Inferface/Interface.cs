07e71422c7fb0bb430d3f00bedd506fdde4864e4
angablade <angablade@gmail.com>
2016-01-17 21:17:00 -0500

    Just added more stuff to the file

--- Plugin Inferface/Interface.cs
+++ Plugin Inferface/Interface.cs
@@ -15,12 +15,39 @@ namespace Plugin_Inferface
         string Description { get; }
         Image icon { get; }
         Form settings_page { get; }
-
         
-
         // testname
         // this is a test plugin for the bot.
         // icon = refrence.image
         // settings_page = form1
+
+        void messagearrived(string message, string username, string channel, DateTime time);
+        void pingarrived(DateTime time);
+        void modearrived(string perams, DateTime time);
+
+        void user_joined(string username, string channel, DateTime time);
+        void user_left(string username, string channel, DateTime time);
+        void user_part(string username, string reason, DateTime time);
+        
+        void bot_channeljoined(string channel, DateTime time);
+        void bot_messagesent(string message, string username, string channel, DateTime time);
+
+        void twitch_commercial_started(DateTime time);
+        void twitch_commercial_ended(DateTime time);
+        void twitch_title_Changed(string title, DateTime time);
+        void twitch_user_followed(string username, DateTime time); 
+
+        void connected(DateTime time);
+        void disconnected(DateTime time);
+        void authorized(DateTime time);
+        void deauthorized(DateTime time);
+        void chatcleared(string chatlog, DateTime time);
+        void slowmode_on(DateTime time);
+        void slowmode_off(DateTime time);
+        void submode_on(DateTime time);
+        void submode_off(DateTime time);
+
+        void plugin_enabled(string plugin_loc, DateTime time);
+        void plugin_disabled(string plugin_loc, DateTime time);
     }
 }
