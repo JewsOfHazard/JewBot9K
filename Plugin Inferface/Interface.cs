be17ed880854180c33daa6c67b324bda0a340354
angablade <angablade@gmail.com>
2016-01-17 20:29:14 -0500

    This is the basic form outline for the bot and the plugins later to come. I only pushed this level of completeion to sync with jew's code at this time.

---
+++ Plugin Inferface/Interface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin_Inferface
+{
+    interface Interface
+    {
+        string Name { get; }
+        string Description { get; }
+        Image icon { get; }
+        Form settings_page { get; }
+
+        // testname
+        // this is a test plugin for the bot.
+        // icon = refrence.image
+        // settings_page = form1
+    }
+}
